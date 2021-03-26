4911a807b9f4177e5762c195807433071b21e94e
尾川貴巳 <>
2021-03-26 18:21:05 +0900

    課題1~5

--- task.rb
+++ task.rb
@@ -6,6 +6,9 @@ def q1
 
   # 以下に回答を記載
 
+  names << ("斎藤")
+  p names
+
 end
 
 def q2
@@ -14,6 +17,10 @@ def q2
 
   # 以下に回答を記載
 
+  array1 << array2
+  array1.flatten!
+  p array1
+
 end
 
 def q3
@@ -21,12 +28,16 @@ def q3
 
   # 以下に回答を記載
 
+  p numbers.count(3)
+
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+
+  sports.delete(nil)
 
   # 以下は変更しないで下さい
   p sports
@@ -37,6 +48,9 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+
+  p array1.empty?
+  p array2.empty?
 
 end
 
